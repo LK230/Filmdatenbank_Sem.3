0e0450e1eb33ea09cf6c47568412e3664498aded
lk230 <lk230@hdm-stuttgart.de>
2024-05-08 15:45:31 +0200

    Create Button Component

---
+++ Frontend/my-app/src/components/buttonComponent/ButtonComponent.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ButtonSVGClose from "../../assets/images/ButtonSVGClose.svg";
+import "./ButtonComponent.css"
+
+export default function ButtonComponent({label}) {
+  return (
+    <div className='ButtonComponentContainer'>
+        
+        <button>
+           <div className='ButtonComponent'>
+        <p>{label}</p>
+        
+          <img
+            src={ButtonSVGClose}
+            alt="open-close Button"
+          />
+       </div> 
+        </button>
+       
+    </div>
+  )
+}
