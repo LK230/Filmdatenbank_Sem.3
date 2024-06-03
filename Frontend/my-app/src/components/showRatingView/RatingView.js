eccc35103dd529bf9d1018142d1dca15ec120bbe
nunu <al.nurefsan03@gmail.com>
2024-06-03 19:45:39 +0200

    added css and added top-container in js

--- Frontend/my-app/src/components/showRatingView/RatingView.js
+++ Frontend/my-app/src/components/showRatingView/RatingView.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import "./RatingView.css"
 
 export default function RatingView({ user, comment, rating}) {
     return (
         <div className='RatingView'>
+        <div className='top-container'>
             <div className='user-container'>
                 <p>{user}</p>
             </div>
+            <div className='rating-container'>
+                <p>{rating}</p>
+            </div>
+        </div>
             <div className='comment-container'>
                 <p>{comment}</p>
-            </div>
-            <div className='rating-container'>
-                
             </div>
     </div>
   )
